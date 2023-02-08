5f1327f45c35efea39a3ca8517fc90f54850b029
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2023-02-08 16:04:25 +0700

    feat: powertools for logging

--- app/main.py
+++ app/main.py
@@ -1,15 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import ExceptionMiddleware
 from mangum import Mangum
 from .utils.power_tools import logger
+from .routers.logging_router import LoggerRouteHandler
 import uvicorn
+import os
 
 app = FastAPI()
+app.router.route_class = LoggerRouteHandler
+app.add_middleware(ExceptionMiddleware, handlers=app.exception_handlers)
+
+
+@app.middleware("http")
+async def add_correlation_id(request: Request, call_next):
+    # Get correlation id from X-Correlation-Id header
+    corr_id = request.headers.get("x-correlation-id")
+    if not corr_id:
+        # If empty, use request id from aws context
+        corr_id = request.scope["aws.context"].aws_request_id
+
+    # Add correlation id to logs
+    logger.set_correlation_id(corr_id)
+
+    response = await call_next(request)
+
+    # Return correlation header in response
+    response.headers["X-Correlation-Id"] = corr_id
+    return response
+
+
+@app.exception_handler(Exception)
+async def unhandled_exception_handler(request, err):
+    logger.exception("Unhandled exception")
+    return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
 
 
 @app.get("/")
 def get_root():
-    print("test")
-    logger.info("Test fastapi with aws powertools")
+    POWERTOOLS_SERVICE_NAME = os.environ.get("POWERTOOLS_SERVICE_NAME")
+    logger.info(f"Test fastapi with aws powertools {POWERTOOLS_SERVICE_NAME}")
     return {"message": "FastAPI running in a lambda function"}
 
 
---
+++ app/routers/logging_router.py
@@ -0,0 +1,23 @@
+from utils.power_tools import logger
+from fastapi.routing import APIRoute
+from typing import Callable
+from fastapi import Request, Response
+
+
+class LoggerRouteHandler(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def route_handler(request: Request) -> Response:
+            # Add fastapi context to logs
+            ctx = {
+                "path": request.url.path,
+                "route": self.path,
+                "method": request.method,
+            }
+            logger.append_keys(fastapi=ctx)
+            logger.info("Received request")
+
+            return await original_route_handler(request)
+
+        return route_handler
